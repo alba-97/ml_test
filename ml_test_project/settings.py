710e864efbbc83c9a3c46b268dc1a52ef7ede3d2
alba-97 <alvarezbasilio97@gmail.com>
2024-11-09 01:22:41 -0300

    Allow onrender.com

--- ml_test_project/settings.py
+++ ml_test_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ml-test.fly.dev']
+ALLOWED_HOSTS = ['ml-test-wp5k.onrender.com']
 
 
 # Application definition
